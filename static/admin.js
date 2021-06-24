c2d66d64b13434923ca7ad763f860bd9419024ca
[qavi-nizamani] <[qaviniz11@gmail.com]>
2021-06-24 16:45:36 +0500

    first commit

---
+++ static/admin.js
@@ -0,0 +1,12 @@
+// Get contacts and insert into admin page..
+(async function getText() {
+  let myObject = await fetch("http://localhost:8000/api/contact");
+  let contact = await myObject.json();
+  let msgBox = document.getElementById("msgBox");
+  let str = "";
+  //   Iterate the contact
+  contact.forEach((contact) => {
+    str += `<div><b>Name:</b> ${contact.name} , <b>Email:</b> ${contact.email} \n<b>Message:</b> ${contact.message}\n</div>`;
+  });
+  msgBox.innerHTML = str;
+})();
